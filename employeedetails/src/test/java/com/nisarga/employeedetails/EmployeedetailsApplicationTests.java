aa1d5cf7863c7ee87de1fae5d1aa64aa2d7ca741
NisargaHG <146320762+NisargaHG@users.noreply.github.com>
2024-08-31 14:30:50 +0530

    Add files via upload

---
+++ employeedetails/src/test/java/com/nisarga/employeedetails/EmployeedetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisarga.employeedetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeedetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
